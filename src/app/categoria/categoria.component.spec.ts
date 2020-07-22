232e945e21cc2bbbd1b96b392be2a9ce38209de7
Pedro Illaisaca <pillaisaca@ups.edu.ec>
2020-07-22 18:47:53 -0500

    typeScript

--- src/app/categoria/categoria.component.spec.ts
+++ src/app/categoria/categoria.component.spec.ts
@@ -22,4 +22,8 @@ describe('CategoriaComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  /* it('should create', () => {
+    expect(component).toBeTruthy();
+  }); */
 });
