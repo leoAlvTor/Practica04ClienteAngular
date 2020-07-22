f03c9dc867bb0184dc8a586adf71f6ecc6cda736
Pedro Illaisaca <pillaisaca@ups.edu.ec>
2020-07-22 18:39:45 -0500

    cambios2

--- karma.conf.js
+++ karma.conf.js
@@ -39,4 +39,11 @@ module.exports = function(config) {
         singleRun: false,
         restartOnFileChange: true
     });
+    /*  plugins: [
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('karma-jasmine-html-reporter'),
+            require('karma-coverage-istanbul-reporter'),
+            require('@angular-devkit/build-angular/plugins/karma')
+        ], */
 };
